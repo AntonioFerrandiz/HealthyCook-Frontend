7257e4cba99b570a56da8c8303f097555e51204f
Antonio Ferrandiz <antoniojfb01@gmail.com>
2022-04-20 09:29:29 -0500

    adding routing

--- src/app/components/menu/home/home.component.ts
+++ src/app/components/menu/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private toastr: ToastrService,
+    private router: Router) { }
 
   ngOnInit(): void {
   }
+  redirectToDespensa():void {
+    this.toastr.info('Esta ventana se encuentra en construcción, vuelve luego!','Holaa!')
+  }
 
+  redirectToRecipes():void {
+    this.router.navigate(['/recipes'])
+  }
+
+  redirectToRestaurants():void {
+    this.router.navigate(['/restaurants'])
+  }
 }
