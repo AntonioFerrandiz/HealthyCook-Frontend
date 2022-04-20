4ffde79cd89187b8f346b9473bac0b3419eb8eab
Antonio Ferrandiz <antoniojfb01@gmail.com>
2022-04-20 15:59:53 -0500

    adding view recipe routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/menu/home/home.component';
 import { RecipesComponent } from './components/menu/home/recipes/recipes.component';
+import { ViewRecipeComponent } from './components/menu/home/recipes/view-recipe/view-recipe.component';
 import { RestaurantsComponent } from './components/menu/home/restaurants/restaurants.component';
 
 const routes: Routes = [
@@ -9,6 +10,7 @@ const routes: Routes = [
 
   { path: 'home', component: HomeComponent },
   { path: 'recipes', component: RecipesComponent },
+  { path: 'recipes/view-recipe/:id', component: ViewRecipeComponent },
   { path: 'restaurants', component: RestaurantsComponent }
 ];
 
