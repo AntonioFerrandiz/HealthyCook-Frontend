2a74a5b0f9a7f29ff1b2bdb7862fbd33862c9792
Antonio Ferrandiz <antoniojfb01@gmail.com>
2022-04-30 22:33:07 -0500

    adding recipe rating functionality

--- src/app/components/menu/home/recipes/view-recipe/view-recipe.component.ts
+++ src/app/components/menu/home/recipes/view-recipe/view-recipe.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { RecipeRating } from 'src/app/models/recipe-rating';
 import { RecipeDetailsService } from 'src/app/services/recipe-details.service';
+import { RecipeRatingService } from 'src/app/services/recipe-rating.service';
 import { RecipeService } from 'src/app/services/recipe.service';
 
 @Component({
@@ -9,29 +13,53 @@ import { RecipeService } from 'src/app/services/recipe.service';
   styleUrls: ['./view-recipe.component.css']
 })
 export class ViewRecipeComponent implements OnInit {
+  starRating = 0; 
+  hovered = 0;
+
   recipeID:number;
   recipe: any[] = []
   recipeDetails: any[] = []
+  ratingRecipeForm = new FormControl(null, Validators.required);
+
   constructor(private recipeService: RecipeService, private recipeDetailsService: RecipeDetailsService,
-    private aRoue: ActivatedRoute) {
+    private aRoue: ActivatedRoute,
+    private fb: FormBuilder,
+    private recipeRatingService: RecipeRatingService,
+    private toastr: ToastrService) {
       this.recipeID = +this.aRoue.snapshot.paramMap.get('id');
+      
      }
 
   ngOnInit(): void {
     this.getRecipe()
     this.getRecipeDetails()
+    
   }
-
+  saveRating():void{
+    if(this.ratingRecipeForm.value == null){
+    } else {
+      const recipeRating: RecipeRating = {
+        recipeID: this.recipeID,
+        rating: this.ratingRecipeForm.value
+      }
+      this.recipeRatingService.saveRecipeRating(recipeRating).subscribe(data =>{
+        this.toastr.success('Gracias por calificar','')
+      }, error =>{
+        console.log(error)
+      })
+    }
+    
+  }
   getRecipe(): void{
     this.recipeService.GetRecipeByID(this.recipeID).subscribe(data =>{
       this.recipe = data
-      console.log(this.recipe)
+      // console.log(this.recipe)
     })
   }
   getRecipeDetails(): void{
     this.recipeDetailsService.GetRecipeDetails(this.recipeID).subscribe(data => {
       this.recipeDetails = data;
-      console.log(this.recipeDetails)
+      // console.log(this.recipeDetails)
     })
   }
 }
