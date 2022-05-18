0e45080775a45384ab78d6b4093d1136e72aaffb
Antonio Ferrándiz <antoniojfb01@gmail.com>
2022-05-18 17:37:48 -0500

    feat - add saved recipes menu

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,6 +1,8 @@
 import { MediaMatcher } from '@angular/cdk/layout';
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { RecipeService } from 'src/app/services/recipe.service';
+import { RecipesSavedService } from 'src/app/services/recipes-saved.service';
 
 @Component({
   selector: 'app-menu',
@@ -9,21 +11,38 @@ import { MatDialog } from '@angular/material/dialog';
 })
 export class MenuComponent implements OnInit {
   mobileQuery: MediaQueryList;
+  recipesSaved: any[] = []
+  recipesName: any[] = []
   private _mobileQueryListener: () => void;
   constructor(changeDetectorRef: ChangeDetectorRef,
-    media: MediaMatcher, public dialog: MatDialog) {
-      this.mobileQuery = media.matchMedia('(max-width: 600px)');
-      this._mobileQueryListener = () => changeDetectorRef.detectChanges();
-      this.mobileQuery.addListener(this._mobileQueryListener);
-     }
+    media: MediaMatcher, public dialog: MatDialog,
+    private recipesSavedService: RecipesSavedService,
+    private recipeService: RecipeService) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
 
-     ngOnInit(): void {}
-
-    ngOnDestroy(): void {
-      this.mobileQuery.removeListener(this._mobileQueryListener);
-    }
-    closeDialog() {
-      const dialogRef = this.dialog.closeAll();
-    }
+  ngOnInit(): void {
+    this.viewRecipesSaved()
+  }
+  viewRecipesSaved(): void {
+    this.recipesSavedService.GetRecipesSavedByUserID(1).subscribe(data => {
+      this.recipesSaved = data
+      for (let i = 0; i < this.recipesSaved.length; i++) {
+        this.recipeService.GetRecipeByID(this.recipesSaved[i].recipeSavedID).subscribe(data => {
+          this.recipesName.push(data?.name)
+          console.log(this.recipesName)
+        })
+        
+      }
+    })
+  }
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+  closeDialog() {
+    const dialogRef = this.dialog.closeAll();
+  }
 
 }
