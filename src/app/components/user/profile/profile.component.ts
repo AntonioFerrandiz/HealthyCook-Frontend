76f6e3c2fd94f9275175921f4e94a0d407710e3a
jack <jack008007z@outlook.es>
2023-11-22 22:57:47 -0500

    feat(add list of followers)

--- src/app/components/user/profile/profile.component.ts
+++ src/app/components/user/profile/profile.component.ts
@@ -7,6 +7,7 @@ import { RecipeService } from 'src/app/services/recipe.service';
 import { RecipesSavedService } from 'src/app/services/recipes-saved.service';
 import { UserServiceService } from 'src/app/services/user-service.service';
 import {FollowerService} from "../../../services/follower.service";
+import {Follower} from "../../../models/follower";
 
 @Component({
   selector: 'app-profile',
@@ -14,7 +15,39 @@ import {FollowerService} from "../../../services/follower.service";
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-  items = [];
+
+  items: Follower[] = [
+    {
+      userID: 1,
+      followerUsername: 'Jack Ronaldo',
+      followedUsername: 'Pedro Lopez',
+      followedUserID: '1fge',
+    },
+    {
+      userID: 1,
+      followerUsername: 'Omar mena',
+      followedUsername: 'Pedro Lopez',
+      followedUserID: '1fge',
+    },
+    {
+      userID: 1,
+      followerUsername: 'Tomas nina',
+      followedUsername: 'Pedro Lopez',
+      followedUserID: '1fge',
+    },
+    {
+      userID: 1,
+      followerUsername: 'Maria arruace',
+      followedUsername: 'Pedro Lopez',
+      followedUserID: '1fge',
+    },
+    {
+      userID: 1,
+      followerUsername: 'Mena peña',
+      followedUsername: 'Pedro Lopez',
+      followedUserID: '1fge',
+    },
+  ];
 
   userData: any[] = []
   dateCreated: String
